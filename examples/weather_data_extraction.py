2b6af44fc576a87e8e77bc6ccd8e3e6a692508d5
alenrajsp <alen.rajsp@um.si>
2021-01-07 17:03:37 +0100

    Added Weather data.
    Added trackpoint timestamps to read_one_file method. Renamed tcx2 to tcx for clarity in tcx_manipulation. Added WeatherIdentification class that can be used to retrieve historical weather data of the completed trainings.

---
+++ examples/weather_data_extraction.py
@@ -0,0 +1,13 @@
+from sport_activities_features.weather_identification import WeatherIdentification
+from sport_activities_features.tcx_manipulation import TCXFile
+
+#read TCX file
+tcx_file = TCXFile()
+tcx_data = tcx_file.read_one_file("path_to_the_file")
+
+#configure visual crossing api key
+visual_crossing_api_key = "API_KEY" # https://www.visualcrossing.com/weather-api
+
+#return weather objects
+weather = WeatherIdentification(tcx_data['positions'], tcx_data['timestamps'], visual_crossing_api_key)
+weatherlist = weather.get_weather()
