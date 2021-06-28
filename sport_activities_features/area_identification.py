4ec5339675ed8b752f8f7a45f378074b40cd09d1
Luka Lukač <luka.lukac@student.um.si>
2021-06-28 13:24:57 +0200

    Implementation of area detection with rays

--- sport_activities_features/area_identification.py
+++ sport_activities_features/area_identification.py
@@ -24,22 +24,45 @@ class AreaIdentifiaction(object):
         self.distances = np.array(distances)
         self.area_coordinates = np.array(area_coordinates)
 
-    def is_point_on_the_right_side_of_line_segment(self, point, linesegment_point1, linesegment_point2) -> bool:
-        """ Checking whether the point is on the right side of the line, given with two points.
+    def is_equal(self, value1, value2) -> bool:
+        """ Checking whether the two values are equal with certain tolerance.
             return: bool
         """
-        # Initializing the two vectors
-        point = np.array(point)
-        vector1 = np.array(point - linesegment_point1)
-        vector2 = np.array(linesegment_point2 - linesegment_point1)
+        tolerance = 0.00001
 
-        cross_product = np.cross(vector1, vector2)  # Calculating the cross product between the two vectors
-
-        # If the cross product is >=0, the point is on the right side of the line or on the line
-        if cross_product >= 0:
+        # If the absolute value of substraction is smaller than the tolerance threshold,
+        # the two values are counted as equal.
+        if abs(value1 - value2) < tolerance:
             return True
-        else:
+        
+        return False
+
+    def do_two_lines_intersect(self, p1, p2, p3, p4) -> bool:
+        """ Checking whether two lines have an intersection point.
+            return: bool
+        """
+        # Initialization of vectors and values.
+        v12 = np.array(p2 - p1)
+        v34 = np.array(p4 - p3)
+        v31 = np.array(p1 - p3)
+        D = np.cross(v12, v34)
+        A = np.cross(v34, v31)
+        B = np.cross(v12, v31)
+        
+        # If D == 0, the two line segments are parallel
+        if self.is_equal(D, 0):
             return False
+
+        Ua = A / D
+        Ub = B / D
+
+        # If the intersection point is in the middle of the line segment, the intersection is counted.
+        if Ua > 0 and Ua < 1 and Ub > 0 and Ub < 1:
+            return True
+        elif self.is_equal(Ua, 0):
+            return True
+        
+        return False
 
     def identify_distance_in_area(self) -> float:
         """ Identifying the distance (in meters) of the activity inside the specified area.
@@ -47,16 +70,18 @@ class AreaIdentifiaction(object):
         """
         distance_in_area = 0.0
 
-        # Checking whether coordinates are inside the given area
+        # Checking whether coordinates are inside the given area.
         for i in np.arange(1, np.shape(self.positions)[0]):
-            # If the point is on the right side of all vectors in polygone (going clockwise) with area end points,
-            # we can assume with certainty that the point is inside that area
-            for j in np.arange(-1, np.shape(self.area_coordinates)[0] - 1):
-                # If point is not on the right side of a vector, we can assume it is not inside the area
-                if not self.is_point_on_the_right_side_of_line_segment(self.positions[i], self.area_coordinates[j], self.area_coordinates[j + 1]) and \
-                   not self.is_point_on_the_right_side_of_line_segment(self.positions[i - 1], self.area_coordinates[j], self.area_coordinates[j + 1]):
-                    break
-            else:
+            number_of_intersections = 0
+
+            # If the ray intersects with the area even times, the point is not inside area.
+            for border in np.arange(np.shape(self.area_coordinates)[0]):
+                for j in np.arange(-1, np.shape(self.area_coordinates[border])[0] - 1):
+                    if self.do_two_lines_intersect(self.area_coordinates[border][j], self.area_coordinates[border][j + 1], np.array([self.positions[i][0], self.positions[i][1]]), np.array([190, self.positions[i][1]])):
+                        number_of_intersections += 1
+
+            # If the number of intersections is odd, the point is inside the given area.
+            if number_of_intersections % 2 == 1:
                 distance_in_area += self.distances[i] - self.distances[i - 1]  # If the point is inside area, distance is added
 
         return distance_in_area
