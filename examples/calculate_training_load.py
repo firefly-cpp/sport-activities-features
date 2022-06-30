907ae780ce37f71fcacff5bbbf0e38591baeac3b
luckyLukec <luka.lukac@student.um.si>
2022-06-30 17:11:00 +0200

    Edwards TRIMP calculation added

--- examples/calculate_training_load.py
+++ examples/calculate_training_load.py
@@ -1,5 +1,25 @@
-from sport_activities_features.training_loads import BanisterTRIMP
+from sport_activities_features.tcx_manipulation import TCXFile
+from sport_activities_features.training_loads import (
+    BanisterTRIMP,
+    EdwardsTRIMP
+)
 
 
-TRIMP = BanisterTRIMP(50, 120)
-print(TRIMP.calculate_TRIMP())
+# Reading a TCX file.
+tcx_file = TCXFile()
+activity = tcx_file.read_one_file('../datasets/15.tcx')
+timestamps = activity['timestamps']
+heart_rates = activity['heartrates']
+
+
+# Calculating Banister TRIMP.
+duration = (timestamps[-1] - timestamps[0]).seconds
+average_heart_rate = sum(filter(None, heart_rates)) / len(heart_rates)
+banister = BanisterTRIMP(duration, average_heart_rate)
+banister_TRIMP = banister.calculate_TRIMP()
+print('Banister TRIMP:', int(banister_TRIMP))
+
+# Calculating Edwards TRIMP.
+edwards = EdwardsTRIMP(heart_rates, timestamps, max_heart_rate=200)
+edwards_TRIMP = edwards.calculate_TRIMP()
+print('Edwards TRIMP: ', edwards_TRIMP)
--- tests/test_training_loads.py
+++ tests/test_training_loads.py
@@ -1,16 +1,62 @@
 from unittest import TestCase
 
-from sport_activities_features.training_loads import BanisterTRIMP
+from sport_activities_features import (
+    BanisterTRIMP,
+    EdwardsTRIMP,
+    TCXFile
+)
 
 
 class BanisterTRIMPTestCase(TestCase):
+    """
+    Banister TRIMP test.
+    """
     def setUp(self):
+        """
+        Setting up the test.
+        """
         self.__banister = BanisterTRIMP(33.44, 165.22)
 
     def test_init_banister_trimp_works_fine(self):
+        """
+        Testing class arguments.
+        """
         self.assertEqual(33.44, self.__banister.duration)
-        self.assertEqual(165.22, self.__banister.avg_hr)
+        self.assertEqual(165.22, self.__banister.average_heart_rate)
 
     def test_calculate_trimp_works_fine(self):
+        """
+        Testing Banister TRIMP.
+        """
         val = self.__banister.calculate_TRIMP()
         self.assertEqual(5524.9568, val)
+
+
+class EdwardsTRIMPTestCase(TestCase):
+    """
+    Edwards TRIMP test.
+    """
+    def setUp(self):
+        """
+        Setting up the test.
+        """
+        tcx_file = TCXFile()
+        activity = tcx_file.read_one_file('../datasets/15.tcx')
+        timestamps = activity['timestamps']
+        heart_rates = activity['heartrates']
+        self.__edwards = EdwardsTRIMP(heart_rates, timestamps, 200)
+
+    def test_init_banister_trimp_works_fine(self):
+        """
+        Testing class arguments.
+        """
+        self.assertEqual(7799, len(self.__edwards.heart_rates))
+        self.assertEqual(7799, len(self.__edwards.timestamps))
+        self.assertEqual(200, self.__edwards.max_heart_rate)
+
+    def test_calculate_trimp_works_fine(self):
+        """
+        Testing Edwards TRIMP.
+        """
+        val = self.__edwards.calculate_TRIMP()
+        self.assertEqual(41458, val)
