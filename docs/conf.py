ce9e634145e27eaae7ec8c80d3ab31eb9c1b006a
firefly-cpp <iztok@iztok-jr-fister.eu>
2022-08-02 22:06:54 +0200

    Bump version

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020-2022, Iztok Fister Jr. et al.'
 author = 'Iztok Fister Jr., Luka Lukač, Alen Rajšp, Luka Pečnik, Dušan Fister'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.1'
+release = '0.3.2'
 
 # -- General configuration ---------------------------------------------------
 
--- sport_activities_features/__init__.py
+++ sport_activities_features/__init__.py
@@ -54,4 +54,4 @@ __all__ = [
     'InterruptionProcessor'
 ]
 
-__version__ = '0.3.1'
+__version__ = '0.3.2'
