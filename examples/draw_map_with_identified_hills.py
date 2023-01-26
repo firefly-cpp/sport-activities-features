1cf8f87357c8acaead5237eb17010c2b7fe93860
kukov.rok@gmail.com <eZD2sBxiyULMsx_ehsur>
2023-01-26 11:08:44 +0100

    update example

--- examples/draw_map_with_identified_hills.py
+++ examples/draw_map_with_identified_hills.py
@@ -5,26 +5,17 @@ from sport_activities_features.hill_identification import HillIdentification
 from sport_activities_features.plot_data import PlotData
 from sport_activities_features.tcx_manipulation import TCXFile
 
-
 # read TCX file
 tcx_file = TCXFile()
-(
-    activity_type,
-    positions,
-    altitudes,
-    distances,
-    total_distance,
-    timestamps,
-    heartrates,
-    speeds
-) = tcx_file.read_one_file('path_to_the_data').values()
+
+data = tcx_file.read_one_file('path_to_the_data')
 
 # detect hills in data
-Hill = HillIdentification(altitudes, 30)
+Hill = HillIdentification(data["altitudes"], 30)
 Hill.identify_hills()
 all_hills = Hill.return_hills()
 
 # draw detected hills
 Map = PlotData()
 
-Map.draw_hills_in_map(altitudes, distances, all_hills)
+Map.draw_hills_in_map(data["altitudes"], data["distances"], all_hills)
