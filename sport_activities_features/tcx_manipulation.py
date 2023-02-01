5a3e08647496a4a657608eab0744587cc17b0bf7
KukovecRok <rok.kukovec1@student.um.si>
2023-02-01 20:00:59 +0100

    Add conversion from tcx to gpx file

--- sport_activities_features/tcx_manipulation.py
+++ sport_activities_features/tcx_manipulation.py
@@ -1,6 +1,8 @@
 import os
 from tcxreader.tcxreader import TCXTrackPoint, TCXReader
 import numpy as np
+from tcx2gpx import tcx2gpx
+from pathlib import Path
 
 from sport_activities_features.file_manipulation import FileManipulation
 
@@ -39,7 +41,8 @@ class TCXFile(FileManipulation):
             filename (str):
                 name of the TCX file to be read
             numpy_array (bool):
-                if set to true dictionary lists are transformed into numpy.arrays
+                if set to true dictionary lists are
+                   transformed into numpy.arrays
         Returns:
             activity = {
                 'activity_type': activity_type,
@@ -71,13 +74,14 @@ class TCXFile(FileManipulation):
         trackpoint: TCXTrackPoint
 
         for index, trackpoint in enumerate(tcx.trackpoints):
-
             if index != 0:
                 delta_distance = trackpoint.distance - distances[-1]
                 delta_time = (trackpoint.time - timestamps[-1]).total_seconds()
-                if (delta_time != 0):
+                if delta_time != 0:
                     heartrates.append(trackpoint.hr_value)
-                    positions.append((trackpoint.latitude, trackpoint.longitude))
+                    positions.append(
+                        (trackpoint.latitude, trackpoint.longitude)
+                    )
                     altitudes.append(trackpoint.elevation)
                     distances.append(trackpoint.distance)
                     timestamps.append(trackpoint.time)
@@ -86,20 +90,19 @@ class TCXFile(FileManipulation):
                 heartrates.append(trackpoint.hr_value)
                 positions.append((trackpoint.latitude, trackpoint.longitude))
                 altitudes.append(trackpoint.elevation)
-                if (trackpoint.distance == None):
+                if trackpoint.distance is None:
                     distances.append(0)
                 else:
                     distances.append(trackpoint.distance)
                 timestamps.append(trackpoint.time)
                 speeds.append(0)
 
-
         try:
             total_distance = tcx.distance
         except BaseException:
             total_distance = None
 
-        if numpy_array == True:
+        if numpy_array is True:
             positions = np.array(positions)
             altitudes = np.array(altitudes)
             distances = np.array(distances)
@@ -108,22 +111,17 @@ class TCXFile(FileManipulation):
             speeds = np.array(speeds)
 
         activity = {
-            'activity_type': activity_type,
-            'positions': positions,
-            'altitudes': altitudes,
-            'distances': distances,
-            'total_distance': total_distance,
-            'timestamps': timestamps,
-            'heartrates': heartrates,
-            'speeds': speeds,
-            'start_time' : tcx.start_time
-
+            "activity_type": activity_type,
+            "positions": positions,
+            "altitudes": altitudes,
+            "distances": distances,
+            "total_distance": total_distance,
+            "timestamps": timestamps,
+            "heartrates": heartrates,
+            "speeds": speeds,
+            "start_time": tcx.start_time,
         }
         return activity
-
-
-
-
 
     def extract_integral_metrics(self, filename: str) -> dict:
         """
@@ -212,23 +210,120 @@ class TCXFile(FileManipulation):
             descent = None
 
         try:
-            steps = tcx.lx_ext['Steps']
+            steps = tcx.lx_ext["Steps"]
         except BaseException:
             steps = None
 
         int_metrics = {
-            'activity_type': activity_type,
-            'distance': distance,
-            'duration': duration,
-            'calories': calories,
-            'hr_avg': hr_avg,
-            'hr_max': hr_max,
-            'hr_min': hr_min,
-            'altitude_avg': altitude_avg,
-            'altitude_max': altitude_max,
-            'altitude_min': altitude_min,
-            'ascent': ascent,
-            'descent': descent,
-            'steps': steps,
+            "activity_type": activity_type,
+            "distance": distance,
+            "duration": duration,
+            "calories": calories,
+            "hr_avg": hr_avg,
+            "hr_max": hr_max,
+            "hr_min": hr_min,
+            "altitude_avg": altitude_avg,
+            "altitude_max": altitude_max,
+            "altitude_min": altitude_min,
+            "ascent": ascent,
+            "descent": descent,
+            "steps": steps,
         }
         return int_metrics
+
+    def write_gpx(self, gps_object, output_file_name=None):
+        """
+        Write GPX object to file.
+        if output_file_name is not specified,
+        the output file name will be the same
+        as the input file name,
+        but with .gpx extension.
+        """
+        if output_file_name is None:
+            output_file_name = str(gps_object.tcx_path.resolve()).replace(
+                ".tcx", ".gpx"
+            )
+
+        with open(output_file_name, "w") as output:
+            output.write(gps_object.gpx.to_xml())
+
+    def create_gps_object(self, path_to_the_file):
+        """
+        Convert TCX file to GPX file.
+        """
+        gps_object = tcx2gpx.TCX2GPX(tcx_path=path_to_the_file)
+        gps_object.read_tcx()
+        gps_object.extract_track_points()
+        gps_object.create_gpx()
+        return gps_object
+
+    def convert_tcx_to_gpx(
+        self, local_path_to_original_file: str, output_file_name=None
+    ):
+        try:
+            """
+            This is a try-except block.
+            First we create a Path objects from the local path
+            Then we try to convert the file.
+            If it fails, we try to fix the file and convert it.
+            """
+            local_path_to_original_file = Path(local_path_to_original_file)
+            abs_path_to_directory = str(
+                os.path.split(os.path.abspath(local_path_to_original_file))[0]
+            )
+            abs_path_to_original_file = str(
+                Path(local_path_to_original_file).resolve()
+            )
+
+            gps_object = self.create_gps_object(abs_path_to_original_file)
+            if output_file_name is None:
+                self.write_gpx(gps_object)
+            else:
+                output_file_name = (
+                    abs_path_to_directory + "\\" + str(output_file_name)
+                )
+                self.write_gpx(gps_object, output_file_name)
+            return gps_object
+
+        except ValueError:
+            try:
+                """
+                This is a try-except block.
+                And simple naive fix for the file.
+                Due to ISO 8601 standard, the Z in
+                the end of the time string is not
+                allowed to have milliseconds.
+                """
+
+                # Intermediate file
+                if output_file_name is None:
+                    path_to_the_fixed_file = str(
+                        abs_path_to_original_file.replace(".tcx", ".gpx")
+                    )
+                else:
+                    path_to_the_fixed_file = (
+                        abs_path_to_directory + "\\" + str(output_file_name)
+                    )
+
+                # Open original file to try fix and convert from tcx to gpx
+                file_to_fix_convert = open(abs_path_to_original_file, "rt")
+
+                # Output file - .gpx - to write the result to
+                fixed_file = open(path_to_the_fixed_file, "wt")
+
+                # For every line of the original file
+                for line in file_to_fix_convert:
+                    # Read and replace string, write to output file
+                    fixed_file.write(line.replace(".000Z", "Z"))
+
+                # Close original and fixed file
+                file_to_fix_convert.close()
+                fixed_file.close()
+
+                gps_object = self.create_gps_object(path_to_the_fixed_file)
+                self.write_gpx(gps_object, path_to_the_fixed_file)
+
+                return gps_object
+
+            except ValueError:
+                raise ValueError("Error in converting the file")
